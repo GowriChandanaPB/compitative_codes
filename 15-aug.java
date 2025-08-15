52eeb675dc5d8ca2da3ec397fbb871feaeacd9ae
Gowri Chandana PB <gowrichandana85@gmail.com>
2025-08-15 20:19:36 +0530

    Check for Balanced Tree

--- 15-aug.java
+++ 15-aug.java
@@ -205,3 +205,21 @@ class Solution {
         return true;
     }
 }
+
+
+// 10. Check for Balanced Tree
+
+class Solution {
+    private int height(Node root){
+        if(root==null)return 0;
+        int l=height(root.left);
+        int r=height(root.right);
+        if(l==-1 || r==-1)return -1;
+        if(Math.abs(l-r)>1)return -1;
+        return Math.max(l,r)+1;
+    }
+    
+    public boolean isBalanced(Node root) {
+        return height(root)!=-1;
+    }
+}
