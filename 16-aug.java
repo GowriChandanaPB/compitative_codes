b38b1f69dc4333425a47067e2962a00c5eb94bdf
Gowri Chandana PB <gowrichandana85@gmail.com>
2025-08-16 20:32:24 +0530

    Duplicate subtree in Binary Tree

--- 16-aug.java
+++ 16-aug.java
@@ -144,3 +144,32 @@ class Tree
             return ans;
       }
 }
+
+
+// 7. Duplicate subtree in Binary Tree
+
+class Solution {
+    HashSet<String> hs=new HashSet<>();
+    int dupSub(Node root) {
+        if(solve(root).equals("#")) return 1;
+        else return 0;
+    }
+    public String solve(Node root)
+    {
+        String s="";
+        if(root==null)
+            return "$";
+        String ls=solve(root.left);
+        if(ls.equals("#"))
+            return "#";
+        String rs=solve(root.right);
+        if(rs.equals("#"))
+            return "#";
+        s=root.data+"/"+ls+"_"+rs;
+        String r=Integer.toString(root.data);
+        if(s.length()>r.length()+4 && hs.contains(s))
+            return "#";
+        hs.add(s);
+        return s;
+    }
+}
