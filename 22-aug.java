7db310e7e222c3330346ce76b56cc5605791e9c5
Gowri Chandana PB <gowrichandana85@gmail.com>
2025-08-22 15:33:14 +0530

    Sliding Window Maximum

--- 22-aug.java
+++ 22-aug.java
@@ -128,3 +128,26 @@ class MedianFinder {
         }
     }
 }
+
+
+// 6. Sliding Window Maximum
+
+class Solution {
+    public ArrayList<Integer> maxOfSubarrays(int[] arr, int k) {
+        ArrayList<Integer> list = new ArrayList<>();
+        ArrayDeque<Integer> Deque = new ArrayDeque<>();
+        for(int i = 0;i<arr.length;i++){
+            if(!Deque.isEmpty() && Deque.getFirst()<=i-k){
+                Deque.removeFirst();
+            }
+            while(!Deque.isEmpty() && arr[Deque.getLast()]<arr[i]){
+                Deque.removeLast();
+            }
+            Deque.addLast(i);
+            if(i>=k-1){
+                list.add(arr[Deque.getFirst()]);
+            }
+        }
+        return list;
+    }
+}
