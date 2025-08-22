8afe9b5e44ae8f8f0aa7b68846a768a81833aa38
Gowri Chandana PB <gowrichandana85@gmail.com>
2025-08-22 15:18:22 +0530

    Connect Ropes

--- 22-aug.java
+++ 22-aug.java
@@ -42,3 +42,25 @@ class Solution {
         return arr;
     }
 };
+
+
+// 3. Connect Ropes
+
+class Solution {
+    public static int minCost(int[] arr) {
+        int minCost = 0;
+        int n = arr.length;
+        PriorityQueue<Integer> pq = new PriorityQueue<>(); 
+        for(int i = 0; i<n;i++){
+            pq.add(arr[i]);
+        }
+        while(pq.size()>1){
+            int first = pq.poll();  
+            int second = pq.poll();  
+            int sum = first+second;
+            minCost += sum; 
+            pq.add(sum);  
+        }
+        return minCost;
+    }
+}
