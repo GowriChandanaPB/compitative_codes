5e550330ffd81ecc94ffeea0aff5a3589ee9c705
Gowri Chandana PB <gowrichandana85@gmail.com>
2025-08-22 15:42:38 +0530

    Find the smallest positive number

--- 22-aug.java
+++ 22-aug.java
@@ -136,6 +136,7 @@ class Solution {
     public ArrayList<Integer> maxOfSubarrays(int[] arr, int k) {
         ArrayList<Integer> list = new ArrayList<>();
         ArrayDeque<Integer> Deque = new ArrayDeque<>();
+        
         for(int i = 0;i<arr.length;i++){
             if(!Deque.isEmpty() && Deque.getFirst()<=i-k){
                 Deque.removeFirst();
@@ -170,7 +171,7 @@ class Solution {
                 return i;
             }
         }
-        
+
         return 1;
     }
 }
