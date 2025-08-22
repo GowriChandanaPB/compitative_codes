da810c1cb80c45e1ac76789c22a9514252967565
Gowri Chandana PB <gowrichandana85@gmail.com>
2025-08-22 15:41:09 +0530

    Find the smallest positive number

--- 22-aug.java
+++ 22-aug.java
@@ -151,3 +151,25 @@ class Solution {
         return list;
     }
 }
+
+
+// 7. Find the smallest positive number
+
+class Solution {
+    public int missingNumber(int[] arr) {
+        Map<Integer,Integer> hp=new TreeMap<>();
+        int int_max=Integer.MAX_VALUE;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]>0){
+                hp.put(arr[i],hp.getOrDefault(arr[i],0)+1);
+            } 
+        }
+        if(hp.size()==0) return 1;
+        for(int i=1;i<int_max;i++){
+            if(!hp.containsKey(i)){
+                return i;
+            }
+        }
+        return 1;
+    }
+}
