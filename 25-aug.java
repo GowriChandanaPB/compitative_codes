59fa6c8376b14fc131e5ed32db5aa0da33f4def4
Gowri Chandana PB <gowrichandana85@gmail.com>
2025-08-25 14:38:17 +0530

    search in rotated sorted array

--- 25-aug.java
+++ 25-aug.java
@@ -91,3 +91,36 @@ class Solution {
         return sum;
     }
 }
+
+
+// 6. search in rotated sorted array
+
+class Solution {
+    public int search(int[] nums, int target) {
+        int l = 0;
+        int h = nums.length - 1;
+
+        while (l <= h) {
+            int m = (l + h) / 2;
+
+            if (nums[m] == target) {
+                return m;
+            }
+            if (nums[l] <= nums[m]) {
+                if (nums[l] <= target && target < nums[m]) {
+                    h = m - 1;
+                } else {
+                    l = m + 1;
+                }
+            } else {
+                if (nums[m] < target && target <= nums[h]) {
+                    l = m + 1;
+                } else {
+                    h = m - 1;
+                }
+            }
+        }
+
+        return -1;
+    }
+}
