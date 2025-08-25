de0157892b209c0b69431079e7ea2a7c3d075623
Gowri Chandana PB <gowrichandana85@gmail.com>
2025-08-25 14:37:41 +0530

    product of array expect self

--- 25-aug.java
+++ 25-aug.java
@@ -69,3 +69,25 @@ class Solution {
         return false; 
     }
 }
+
+
+// 5. product of array expect self
+
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int n = nums.length; 
+        int left = 1;
+        int right = 1;
+        int [] sum = new int[n];
+
+        for(int i=n-1; i>=0; i--){
+            sum[i] = right;
+            right = right*nums[i];
+        }
+        for(int i=0; i<n; i++){
+            sum[i] = sum[i] * left;
+            left = left * nums[i];
+        }
+        return sum;
+    }
+}
