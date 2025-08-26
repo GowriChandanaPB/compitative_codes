ff697839a4e3baf133b46520e0db14d2a9b4a5f0
Gowri Chandana PB <gowrichandana85@gmail.com>
2025-08-26 16:36:38 +0530

    Steps by Knight

--- 26-aug.java
+++ 26-aug.java
@@ -31,4 +31,45 @@ class Solution {
     private boolean isSafe(int row, int col, int[][] maze, boolean[][] visited, int n) {
         return row >= 0 && col >= 0 && row < n && col < n && maze[row][col] == 1 && !visited[row][col];
     }
+}
 
+
+// 2. Steps by Knight
+
+class Solution {
+    
+    static int[] dx = {2, 2, -2, -2, 1, 1, -1, -1};
+    static int[] dy = {1, -1, 1, -1, 2, -2, 2, -2};
+    
+    public  int minStepToReachTarget(int knightPos[], int targetPos[], int N) {
+        if (Arrays.equals(knightPos, targetPos)) {
+            return 0;
+        }
+        
+        Queue<int[]> queue = new LinkedList<>();
+        queue.offer(new int[] {knightPos[0], knightPos[1], 0});  // (x, y, steps)
+        
+        boolean[][] visited = new boolean[N + 1][N + 1];
+        visited[knightPos[0]][knightPos[1]] = true;
+
+        while (!queue.isEmpty()) {
+            int[] current = queue.poll();
+            int x = current[0], y = current[1], steps = current[2];
+            
+            for (int i = 0; i < 8; i++) {
+                int newX = x + dx[i], newY = y + dy[i];
+                
+                if (newX >= 1 && newX <= N && newY >= 1 && newY <= N && !visited[newX][newY]) {
+                    if (newX == targetPos[0] && newY == targetPos[1]) {
+                        return steps + 1;
+                    }
+                    
+                    visited[newX][newY] = true;
+                    queue.offer(new int[] {newX, newY, steps + 1});
+                }
+            }
+        }
+        
+        return -1;
+    }
+}
