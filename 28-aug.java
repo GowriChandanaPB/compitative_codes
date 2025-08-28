32ee98b735dbf0d40c87a45e9932029db3200ce1
Gowri Chandana PB <gowrichandana85@gmail.com>
2025-08-28 16:29:00 +0530

    Floyd Warshall

--- 28-aug.java
+++ 28-aug.java
@@ -29,3 +29,21 @@ class Solution
        return 0;
     }
 }
+
+
+// 2. Floyd Warshall
+
+class Solution {
+    public void floydWarshall(int[][] dist) {
+        int n = dist.length;
+        for(int k=0; k<n; k++) {
+            for(int i=0; i<n; i++) {
+                for(int j=0; j<n; j++) {
+                    if(dist[i][k] != (int)1e8 && dist[k][j] != (int) 1e8) {
+                        dist[i][j] = Math.min(dist[i][k] + dist[k][j], dist[i][j]);
+                    }
+                }
+            }
+        }
+    }
+}
