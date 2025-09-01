a7eb137888c17ea626d7444d44d7a2bfa6c66093
Gowri Chandana PB <gowrichandana85@gmail.com>
2025-09-01 23:12:35 +0530

    Product of Array Except Self

--- 1-sep.java
+++ 1-sep.java
@@ -121,3 +121,25 @@ public class Solution {
         return res;
     }
 }
+
+
+// 7. Product of Array Except Self   
+
+public class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int n = nums.length;
+        int[] res = new int[n];
+
+        res[0] = 1;
+        for (int i = 1; i < n; i++) {
+            res[i] = res[i - 1] * nums[i - 1];
+        }
+
+        int postfix = 1;
+        for (int i = n - 1; i >= 0; i--) {
+            res[i] *= postfix;
+            postfix *= nums[i];
+        }
+        return res;
+    }
+}
