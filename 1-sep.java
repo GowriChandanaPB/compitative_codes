63ee9b0cd4c6bf367c22cebc7314eee68fc2e4cf
Gowri Chandana PB <gowrichandana85@gmail.com>
2025-09-01 22:27:57 +0530

    Valid Anagram

--- 1-sep.java
+++ 1-sep.java
@@ -12,3 +12,19 @@ class Solution {
         return false;
     }
 }
+
+
+// 2. Valid Anagram   
+
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length() != t.length()){
+            return false;
+        }
+        char[] sArr = s.toCharArray();
+        char[] tArr = t.toCharArray();
+        Arrays.sort(sArr);
+        Arrays.sort(tArr);
+        return Arrays.equals(sArr, tArr);
+    }
+}
