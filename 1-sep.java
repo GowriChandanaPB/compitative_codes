67868a89742fe747551cba1a8dc8cfb09267d1c2
Gowri Chandana PB <gowrichandana85@gmail.com>
2025-09-01 22:51:14 +0530

    Group Anagrams

--- 1-sep.java
+++ 1-sep.java
@@ -45,3 +45,24 @@ class Solution {
         return new int[]{-1, -1};
     }
 }
+
+
+// 4. Group Anagrams   
+
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> ans = new HashMap<>();
+
+        for (String s : strs) {
+            char[] chars = s.toCharArray();
+            Arrays.sort(chars);
+            String key = new String(chars);
+            if (!ans.containsKey(key)) {
+                ans.put(key, new ArrayList<>());
+            }
+            ans.get(key).add(s);
+        }
+
+        return new ArrayList<>(ans.values());        
+    }
+}
