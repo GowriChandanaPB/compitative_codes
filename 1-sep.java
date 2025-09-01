23f9605049081546197ae185ff1095cb5ac26491
Gowri Chandana PB <gowrichandana85@gmail.com>
2025-09-01 23:02:04 +0530

    Encode and Decode Strings

--- 1-sep.java
+++ 1-sep.java
@@ -90,3 +90,34 @@ public class Solution {
         return ans;
     }
 }
+
+
+// 6. Encode and Decode Strings   
+
+public class Solution {
+
+    public String encode(List<String> strs) {
+        StringBuilder ans = new StringBuilder();
+        for (String s : strs) {
+            ans.append(s.length()).append('#').append(s);
+        }
+        return ans.toString();
+    }
+
+    public List<String> decode(String str) {
+        List<String> res = new ArrayList<>();
+        int i = 0;
+        while (i < str.length()) {
+            int j = i;
+            while (str.charAt(j) != '#') {
+                j++;
+            }
+            int n = Integer.parseInt(str.substring(i, j));
+            i = j + 1;
+            j = i + n;
+            res.add(str.substring(i, j));
+            i = j;
+        }
+        return res;
+    }
+}
