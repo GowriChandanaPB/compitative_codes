0f76b8489540e1df4651f673de934623fdfb8b88
Gowri Chandana PB <gowrichandana85@gmail.com>
2025-09-03 22:11:38 +0530

    min Stack

--- 3-sep.java
+++ 3-sep.java
@@ -22,3 +22,43 @@ class Solution {
         return stack.isEmpty();
     }
 }
+
+
+// 2 min Stack. 
+
+class MinStack {
+
+    private Stack<Integer> stack;
+
+    public MinStack() {
+        stack = new Stack<>();
+    }
+
+    public void push(int val) {
+        stack.push(val);
+    }
+
+    public void pop() {
+        stack.pop();
+    }
+
+    public int top() {
+        return stack.peek();
+    }
+
+    public int getMin() {
+        Stack<Integer> tmp = new Stack<>();
+        int min = stack.peek();
+
+        while (!stack.isEmpty()) {
+            min = Math.min(min, stack.peek());
+            tmp.push(stack.pop());
+        }
+
+        while (!tmp.isEmpty()) {
+            stack.push(tmp.pop());
+        }
+
+        return min;
+    }
+}
