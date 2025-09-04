8c66963f25ab6ce8ddcc305b13c60766d5324a6a
Gowri Chandana PB <gowrichandana85@gmail.com>
2025-09-04 22:52:48 +0530

    Daily Temperatures

--- 4-sep.java
+++ 4-sep.java
@@ -23,4 +23,20 @@ public class Solution {
 }
 
 
-// 2.
+// 2. Daily Temperatures
+
+class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+        int[] res = new int[temperatures.length];
+        Stack<int[]> stack = new Stack<>();
+        for(int i=0; i<temperatures.length; i++){
+            int a = temperatures[i];
+            while(!stack.isEmpty() && a>stack.peek()[0]){
+                int[] pair = stack.pop();
+                res[pair[1]] = i - pair[1];
+            } 
+            stack.push(new int []{a, i});
+        }
+        return res;
+    }
+}
