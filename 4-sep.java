4a2c098b3241a333db8519a49d9c92a7bff3f3a8
Gowri Chandana PB <gowrichandana85@gmail.com>
2025-09-04 22:59:02 +0530

    Car Fleet

--- 4-sep.java
+++ 4-sep.java
@@ -40,3 +40,29 @@ class Solution {
         return res;
     }
 }
+
+
+// 3. Car Fleet
+
+class Solution {
+    public int carFleet(int target, int[] position, int[] speed) {
+        int n = position.length;
+        double[][] car = new double[n][2];
+        for(int i=0; i<n; i++){
+            car[i][0] = position[i];
+            car[i][1] = (double)(target - position[i]) / speed[i];
+        }
+        Arrays.sort(car, (a, b) -> Double.compare(b[0], a[0]));
+        int fleet = 0;
+        double time = 0;
+        for(int i=0; i<n; i++){
+            if(car[i][1] > time){
+                fleet++;
+                time = car[i][1];
+            }
+        }
+        return fleet;
+    }
+}
+
+
