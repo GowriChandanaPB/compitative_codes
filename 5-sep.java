01af0180124f40af4b08132481e3e03a31995740
Gowri Chandana PB <gowrichandana85@gmail.com>
2025-09-05 22:48:00 +0530

    Search a 2D Matrix

---
+++ 5-sep.java
@@ -0,0 +1,21 @@
+// 1. Search a 2D Matrix
+
+public class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int row = matrix.length, col = matrix[0].length;
+
+        int l = 0, r = row * col - 1;
+        while (l <= r) {
+            int m = l + (r - l) / 2;
+            int rw = m / col, cl = m % col;
+            if (target > matrix[rw][cl]) {
+                l = m + 1;
+            } else if (target < matrix[rw][cl]) {
+                r = m - 1;
+            } else {
+                return true;
+            }
+        }
+        return false;
+    }
+}
