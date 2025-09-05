a1e5f77f8b8b058dd733b61808910af7d23d7e62
Gowri Chandana PB <gowrichandana85@gmail.com>
2025-09-06 00:14:37 +0530

    Time Based Key-Value Store

--- 5-sep.java
+++ 5-sep.java
@@ -105,3 +105,27 @@ class Solution {
         return -1;
     }
 }
+
+
+// 5. Time Based Key-Value Store
+
+
+public class TimeMap {
+    private Map<String, TreeMap<Integer, String>> m;
+
+    public TimeMap() {
+        m = new HashMap<>();
+    }
+
+    public void set(String key, String value, int timestamp) {
+        m.computeIfAbsent(key, k -> new TreeMap<>()).put(timestamp, value);
+    }
+
+    public String get(String key, int timestamp) {
+        if (!m.containsKey(key)) return "";
+        TreeMap<Integer, String> timestamps = m.get(key);
+        Map.Entry<Integer, String> entry = timestamps.floorEntry(timestamp);
+        return entry == null ? "" : entry.getValue();
+    }
+}
+
