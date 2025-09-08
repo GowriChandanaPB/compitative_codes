742d71f5590555aa8e52b498907d825abc84c8f6
Gowri Chandana PB <gowrichandana85@gmail.com>
2025-09-08 19:42:25 +0530

    Copy List with Random Pointer

--- 8-sep.java
+++ 8-sep.java
@@ -62,3 +62,24 @@ class Solution {
         return temp.next;
     }
 }
+
+
+// 4. Copy List with Random Pointer
+
+class Solution {
+     HashMap<Node, Node> map = new HashMap<>();
+    public Node copyRandomList(Node head) {
+       
+        if(head == null){
+            return null;
+        }
+        if(map.containsKey(head)){
+            return map.get(head);
+        }
+        Node temp = new Node(head.val);
+        map.put(head, temp);
+        temp.next = copyRandomList(head.next);
+        temp.random = map.get(head.random);
+        return temp;
+    }
+}
