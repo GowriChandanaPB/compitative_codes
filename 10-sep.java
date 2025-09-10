e79c1771be0f29ddbcc3edfc00421f8c3bb4bd15
Gowri Chandana PB <gowrichandana85@gmail.com>
2025-09-10 23:55:15 +0530

    Invert Binary Tree

--- 10-sep.java
+++ 10-sep.java
@@ -133,3 +133,21 @@ class Solution {
         return output;
     }
 }
+
+
+// 5. Invert Binary Tree
+
+class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) return null;
+
+        TreeNode temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+
+        invertTree(root.left);
+        invertTree(root.right);
+
+        return root;
+    }
+}
