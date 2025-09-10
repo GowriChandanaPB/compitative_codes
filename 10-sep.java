2943bb6a9ad1e7239b03f5c0d21419e38ddc19d3
Gowri Chandana PB <gowrichandana85@gmail.com>
2025-09-10 23:44:13 +0530

    Minimum Window Substring

--- 10-sep.java
+++ 10-sep.java
@@ -67,3 +67,49 @@ class Solution {
         return match == 26;
     }
 }
+
+
+// 3. Minimum Window Substring
+
+class Solution {
+    public String minWindow(String s, String t) {
+        if (t.isEmpty()) return "";
+
+        Map<Character, Integer> countT = new HashMap<>();
+        Map<Character, Integer> window = new HashMap<>();
+        for (char i : t.toCharArray()) {
+            countT.put(i, countT.getOrDefault(i, 0) + 1);
+        }
+
+        int have = 0, need = countT.size();
+        int[] res = {-1, -1};
+        int resLen = Integer.MAX_VALUE;
+        int l = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            window.put(c, window.getOrDefault(c, 0) + 1);
+
+            if (countT.containsKey(c) && window.get(c).equals(countT.get(c))) {
+                have++;
+            }
+
+            while (have == need) {
+                if ((i - l + 1) < resLen) {
+                    resLen = i - l + 1;
+                    res[0] = l;
+                    res[1] = i;
+                }
+
+                char leftChar = s.charAt(l);
+                window.put(leftChar, window.get(leftChar) - 1);
+                if (countT.containsKey(leftChar) && window.get(leftChar) < countT.get(leftChar)) {
+                    have--;
+                }
+                l++;
+            }
+        }
+
+        return resLen == Integer.MAX_VALUE ? "" : s.substring(res[0], res[1] + 1);
+    }
+}
