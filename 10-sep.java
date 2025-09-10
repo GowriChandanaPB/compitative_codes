cda3246ee0748d0d83fabb8a754876162cbe268b
Gowri Chandana PB <gowrichandana85@gmail.com>
2025-09-10 23:51:21 +0530

    Sliding Window Maximum

--- 10-sep.java
+++ 10-sep.java
@@ -113,3 +113,23 @@ class Solution {
         return resLen == Integer.MAX_VALUE ? "" : s.substring(res[0], res[1] + 1);
     }
 }
+
+
+// 4. Sliding Window Maximum
+
+class Solution {
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        int n = nums.length;
+        int[] output = new int[n - k + 1];
+
+        for (int i = 0; i <= n - k; i++) {
+            int max = nums[i];
+            for (int j = i; j < i + k; j++) {
+                max = Math.max(max, nums[j]);
+            }
+            output[i] = max;
+        }
+
+        return output;
+    }
+}
