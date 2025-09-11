39e4471adcde9bb149d947d442e84a0d2bda0355
Gowri Chandana PB <gowrichandana85@gmail.com>
2025-09-11 23:52:54 +0530

    Subtree of Another Tree

--- 11-sep.java
+++ 11-sep.java
@@ -50,4 +50,20 @@ class Solution {
 
 // 4. Subtree of Another Tree
 
+class Solution {
+    public boolean isSubtree(TreeNode root, TreeNode subRoot) {
+        if(root == null) return false;
+        if(isSameTree(root, subRoot)) return true;
+        return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
+    }
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if(p == null && q == null) return true;
+        if(p != null && q != null && p.val == q.val){
+            return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+        } else {
+            return false;
+        }
+    }
+}
+
 
