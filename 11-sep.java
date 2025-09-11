335cb77ecb480cb65aca0a1ba88dc69ea1a1367c
Gowri Chandana PB <gowrichandana85@gmail.com>
2025-09-11 23:57:51 +0530

    Lowest Common Ancestor in Binary Search Tree

--- 11-sep.java
+++ 11-sep.java
@@ -69,3 +69,16 @@ class Solution {
 
 // 5. Lowest Common Ancestor in Binary Search Tree
 
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if(root == null) return null;
+        if(p.val < root.val && q.val < root.val){
+            return lowestCommonAncestor(root.left, p, q);
+        } else if(p.val > root.val && q.val > root.val){
+            return lowestCommonAncestor(root.right, p, q);
+        } else {
+            return root;
+        }
+    }
+}
+
