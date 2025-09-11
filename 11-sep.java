16e7dc595c0d13d79e62fc6da7d70dbbe717a92f
Gowri Chandana PB <gowrichandana85@gmail.com>
2025-09-12 00:15:09 +0530

    Binary Tree Level Order Traversal

--- 11-sep.java
+++ 11-sep.java
@@ -85,5 +85,18 @@ class Solution {
 
 // 6. Binary Tree Level Order Traversal
 
-
+class Solution {
+    List<List<Integer>> res = new ArrayList<>();
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        dfs(root, 0);
+        return res;
+    }
+    public void dfs(TreeNode node, int depth){
+        if (node == null) return;
+        if(res.size() == depth) res.add(new ArrayList<>());
+        res.get(depth).add(node.val);
+        dfs(node.left, depth + 1);
+        dfs(node.right, depth + 1);
+    }
+}
 
