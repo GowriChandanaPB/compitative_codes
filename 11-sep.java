4b1d68678d6f36a06a77ca8bb9d4de18574afbcb
Gowri Chandana PB <gowrichandana85@gmail.com>
2025-09-11 23:30:36 +0530

    Balanced Binary Tree

--- 11-sep.java
+++ 11-sep.java
@@ -1,1 +1,19 @@
 // 1. Diameter of Binary Tree
+
+class Solution {
+    public int diameterOfBinaryTree(TreeNode root) {
+        int[] res = new int[1];
+        dfs(root, res);
+        return res[0];
+    }
+    public int dfs(TreeNode root, int[] res){
+        if(root == null) return 0;
+        int left = dfs(root.left, res);
+        int right = dfs(root.right, res);
+        res[0] = Math.max(res[0], left + right);
+        return 1 + Math.max(left, right);
+    }
+}
+
+
+// 2. Balanced Binary Tree
