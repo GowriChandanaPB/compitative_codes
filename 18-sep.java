8b65857425babbceddfb616dc498dbdf4b45cf3e
Gowri Chandana PB <gowrichandana85@gmail.com>
2025-09-19 00:13:44 +0530

    Subsets

--- 18-sep.java
+++ 18-sep.java
@@ -41,3 +41,24 @@ class Solution {
         }
     }
 }
+
+
+// 2. Subsets
+
+class Solution {
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        res.add(new ArrayList<>());
+
+        for (int num : nums) {
+            int size = res.size();
+            for (int i = 0; i < size; i++) {
+                List<Integer> subset = new ArrayList<>(res.get(i));
+                subset.add(num);
+                res.add(subset);
+            }
+        }
+
+        return res;
+    }
+}
