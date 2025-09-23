9cd2fa8708f2cef7f102010fcf8bfffd3e432ebf
Gowri Chandana PB <gowrichandana85@gmail.com>
2025-09-23 23:28:15 +0530

    Combination Sum II

--- 23-sep.java
+++ 23-sep.java
@@ -27,3 +27,30 @@ class Solution {
 
 
 // 2. Combination Sum II
+
+class Solution {
+    List<List<Integer>> res;
+    public List<List<Integer>> combinationSum2(int[] nums, int target) {
+        res = new ArrayList<List<Integer>>();
+        Arrays.sort(nums);
+        List<Integer> cur = new ArrayList();
+        backtrack(nums, target, cur, 0);
+        return res;
+    }
+
+    public void backtrack(int[] nums, int target, List<Integer> cur, int i) {
+        if (target == 0) {
+            res.add(new ArrayList(cur));
+            return;
+        }
+        if (target < 0 || i >= nums.length) {
+            return;
+        }
+
+        cur.add(nums[i]);
+        backtrack(nums, target - nums[i], cur, i + 1);
+        cur.remove(cur.size() - 1);
+        while (i + 1 < nums.length && nums[i] == nums[i + 1]) i++;
+        backtrack(nums, target, cur, i + 1);
+    }
+}
