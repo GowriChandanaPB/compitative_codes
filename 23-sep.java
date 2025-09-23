c1f09151cee72b7571147172f08075762aa8d11a
Gowri Chandana PB <gowrichandana85@gmail.com>
2025-09-24 00:08:31 +0530

    Word Search

--- 23-sep.java
+++ 23-sep.java
@@ -82,3 +82,38 @@ class Solution {
 
 // 4. Word Search
 
+class Solution {
+    private int row, col;
+
+    public boolean exist(char[][] board, String word) {
+        row = board.length;
+        col = board[0].length;
+
+        for (int r = 0; r < row; r++) {
+            for (int c = 0; c < col; c++) {
+                if (dfs(board, word, r, c, 0)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    private boolean dfs(char[][] board, String word, int r, int c, int i) {
+        if (i == word.length()) {
+            return true;
+        }
+        if (r < 0 || c < 0 || r >= row || c >= col ||
+            board[r][c] != word.charAt(i) || board[r][c] == '#') {
+            return false;
+        }
+
+        board[r][c] = '#';
+        boolean res = dfs(board, word, r + 1, c, i + 1) ||
+                      dfs(board, word, r - 1, c, i + 1) ||
+                      dfs(board, word, r, c + 1, i + 1) ||
+                      dfs(board, word, r, c - 1, i + 1);
+        board[r][c] = word.charAt(i);
+        return res;
+    }
+}
