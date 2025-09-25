c898d4cd2ad8b09f4c68e22fa8511cd815abbff8
Gowri Chandana PB <gowrichandana85@gmail.com>
2025-09-25 23:27:41 +0530

    Merge k Sorted Lists

--- 25-sep.java
+++ 25-sep.java
@@ -144,3 +144,28 @@ class Solution {
         return temp;
     }
 }
+
+
+// 6. Merge k Sorted Lists
+
+
+class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+        List<Integer> node = new ArrayList<>();
+        for (ListNode lst : lists) {
+            while (lst != null) {
+                node.add(lst.val);
+                lst = lst.next;
+            }
+        }
+        Collections.sort(node);
+
+        ListNode res = new ListNode(0);
+        ListNode temp = res;
+        for (int i : node) {
+            temp.next = new ListNode(i);
+            temp = temp.next;
+        }
+        return res.next;
+    }
+}
