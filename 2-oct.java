18e55573df07ee1b69ed1fd6ae100cbb1bbc56be
Gowri Chandana PB <gowrichandana85@gmail.com>
2025-10-02 23:25:10 +0530

    Bit Manipulation

--- 2-oct.java
+++ 2-oct.java
@@ -12,3 +12,14 @@ class Solution {
 
 
 // 2. Bit Manipulation
+
+public class Solution {
+    public int reverseBits(int n) {
+        int res = 0;
+        for (int i = 0; i < 32; i++) {
+            int bit = (n >> i) & 1;
+            res += (bit << (31 - i));
+        }
+        return res;
+    }
+}
