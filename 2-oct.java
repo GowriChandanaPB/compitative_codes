7e5918df67cc5c1d2b89d89e4fa3f79136b52094
Gowri Chandana PB <gowrichandana85@gmail.com>
2025-10-02 23:21:51 +0530

    counting bits

--- 2-oct.java
+++ 2-oct.java
@@ -1,1 +1,11 @@
 // 1. Counting Bits
+
+class Solution {
+    public int[] countBits(int n) {
+        int[] res = new int[n + 1];
+        for (int i = 0; i <= n; i++) {
+            res[i] = Integer.bitCount(i);
+        }
+        return res;
+    }
+}
