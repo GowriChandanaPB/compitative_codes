121567273960114c7a46c29674e886e9cfd6563d
Gowri Chandana PB <gowrichandana85@gmail.com>
2025-10-03 23:14:45 +0530

    3oct

--- 2-oct.java
+++ 2-oct.java
@@ -51,3 +51,21 @@ class Solution {
         return a + b;
     }
 }
+
+
+// 6. Reverse Integer
+
+class Solution {
+    public int reverse(int x) {
+        long rev = 0;
+        while (x != 0) {
+            int digit = x % 10;
+            rev = rev * 10 + digit;
+            x /= 10;
+        }
+        if (rev < Integer.MIN_VALUE || rev > Integer.MAX_VALUE) {
+            return 0;
+        }
+        return (int) rev;
+    }
+}
