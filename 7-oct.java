e2a8fc966a1d0ca7deb2b4f4732ec8ba1848a4d7
Gowri Chandana PB <gowrichandana85@gmail.com>
2025-10-07 23:23:27 +0530

    Word Search II

--- 7-oct.java
+++ 7-oct.java
@@ -1,7 +1,7 @@
 // 1. Implement Trie (Prefix Tree)
 
 class TrieNode {
-    TrieNode[] children = new TrieNode[26];
+    TrieNode[] child = new TrieNode[26];
     boolean endOfWord = false;
 }
 
@@ -16,10 +16,10 @@ class PrefixTree {
         TrieNode cur = root;
         for (char c : word.toCharArray()) {
             int i = c - 'a';
-            if (cur.children[i] == null) {
-                cur.children[i] = new TrieNode();
+            if (cur.child[i] == null) {
+                cur.child[i] = new TrieNode();
             }
-            cur = cur.children[i];
+            cur = cur.child[i];
         }
         cur.endOfWord = true;
     }
@@ -81,5 +81,45 @@ class WordDictionary {
             }
         }
         return false;
+    }
+}
+
+
+// 3. Word Search II
+
+class Solution {
+    public List<String> findWords(char[][] board, String[] words) {
+        int row = board.length, COLS = board[0].length;
+        List<String> res = new ArrayList<>();
+
+        for (String word : words) {
+            boolean flag = false;
+            for (int r = 0; r < row && !flag; r++) {
+                for (int c = 0; c < COLS; c++) {
+                    if (board[r][c] != word.charAt(0)) continue;
+                    if (backtrack(board, r, c, word, 0)) {
+                        res.add(word);
+                        flag = true;
+                        break;
+                    }
+                }
+            }
+        }
+        return res;
+    }
+
+    private boolean backtrack(char[][] board, int r, int c, String word, int i) {
+        if (i == word.length()) return true;
+        if (r < 0 || c < 0 || r >= board.length ||
+            c >= board[0].length || board[r][c] != word.charAt(i))
+            return false;
+
+        board[r][c] = '*';
+        boolean ret = backtrack(board, r + 1, c, word, i + 1) ||
+                      backtrack(board, r - 1, c, word, i + 1) ||
+                      backtrack(board, r, c + 1, word, i + 1) ||
+                      backtrack(board, r, c - 1, word, i + 1);
+        board[r][c] = word.charAt(i);
+        return ret;
     }
 }
