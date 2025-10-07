0cb0a315f4483e1635b290e35c00eae2152c8a1c
Gowri Chandana PB <gowrichandana85@gmail.com>
2025-10-07 23:12:17 +0530

    Design Add and Search Word Data Structure

--- 7-oct.java
+++ 7-oct.java
@@ -52,3 +52,34 @@ class PrefixTree {
 
 // 2. Design Add and Search Word Data Structure
 
+class WordDictionary {
+
+    private List<String> store;
+
+    public WordDictionary() {
+        store = new ArrayList<>();
+    }
+
+    public void addWord(String word) {
+        store.add(word);
+    }
+
+    public boolean search(String word) {
+        for (String w : store) {
+            if (w.length() != word.length()) continue;
+            int i = 0;
+            while (i < w.length()) {
+                if (w.charAt(i) == word.charAt(i) ||
+                    word.charAt(i) == '.') {
+                    i++;
+                } else {
+                    break;
+                }
+            }
+            if (i == w.length()) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
