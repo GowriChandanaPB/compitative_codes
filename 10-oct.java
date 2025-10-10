593d32a41d36f13feea78956bac8161af4b1a2fa
Gowri Chandana PB <gowrichandana85@gmail.com>
2025-10-10 23:12:00 +0530

    Course Schedule II

--- 10-oct.java
+++ 10-oct.java
@@ -39,38 +39,43 @@ class Solution {
 // 2. Course Schedule II
 
 class Solution {
+    private List<Integer> output = new ArrayList<>();
+    private int[] indegree;
+    private List<List<Integer>> adj;
+
+    private void dfs(int node) {
+        output.add(node);
+        indegree[node]--;
+        for (int nei : adj.get(node)) {
+            indegree[nei]--;
+            if (indegree[nei] == 0) {
+                dfs(nei);
+            }
+        }
+    }
+
     public int[] findOrder(int numCourses, int[][] prerequisites) {
-        int[] indegree = new int[numCourses];
-        List<List<Integer>> adj = new ArrayList<>();
+        adj = new ArrayList<>();
         for (int i = 0; i < numCourses; i++) {
             adj.add(new ArrayList<>());
         }
+        indegree = new int[numCourses];
         for (int[] pre : prerequisites) {
-            indegree[pre[1]]++;
-            adj.get(pre[0]).add(pre[1]);
+            indegree[pre[0]]++;
+            adj.get(pre[1]).add(pre[0]);
         }
 
-        Queue<Integer> q = new LinkedList<>();
         for (int i = 0; i < numCourses; i++) {
             if (indegree[i] == 0) {
-                q.add(i);
+                dfs(i);
             }
         }
 
-        int finish = 0;
-        int[] ans = new int[numCourses];
-        while (!q.isEmpty()) {
-            int node = q.poll();
-            ans[finish++] = node;
-            for (int nei : adj.get(node)) {
-                indegree[nei]--;
-                if (indegree[nei] == 0) {
-                    q.add(nei);
-                }
-            }
+        if (output.size() != numCourses) return new int[0];
+        int[] res = new int[output.size()];
+        for (int i = 0; i < output.size(); i++) {
+            res[i] = output.get(i);
         }
-
-        if (finish != numCourses) return new int[0];
-        return ans;
+        return res;
     }
 }
