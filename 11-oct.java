b56551459a7eeba61371c8141bb7a4dc8cbef7aa
Gowri Chandana PB <gowrichandana85@gmail.com>
2025-10-12 00:39:09 +0530

    Happy Number

--- 11-oct.java
+++ 11-oct.java
@@ -70,3 +70,33 @@ class Solution {
         }
     }
 }
+
+
+// 4. Happy Number
+
+public class Solution {
+    public boolean isHappy(int n) {
+        Set<Integer> visit = new HashSet<>();
+
+        while (!visit.contains(n)) {
+            visit.add(n);
+            n = squares(n);
+            if (n == 1) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private int squares(int n) {
+        int res = 0;
+
+        while (n > 0) {
+            int digit = n % 10;
+            digit = digit * digit;
+            res += digit;
+            n /= 10;
+        }
+        return res;
+    }
+}
