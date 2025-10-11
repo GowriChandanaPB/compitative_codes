76db1b0a2fc2be7037457e0e0a67c2f25d16771f
Gowri Chandana PB <gowrichandana85@gmail.com>
2025-10-12 00:33:12 +0530

    Spiral Matrix

--- 11-oct.java
+++ 11-oct.java
@@ -18,3 +18,27 @@ class Solution {
         }
     }
 }
+
+
+
+// 2. Spiral Matrix
+
+class Solution {
+    public List<Integer> spiralOrder(int[][] matrix) {
+        List<Integer> res = new ArrayList<>();
+        int[][] dir = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+        int[] step = {matrix[0].length, matrix.length - 1};
+
+        int r = 0, c = -1, d = 0;
+        while (step[d % 2] > 0) {
+            for (int i = 0; i < step[d % 2]; i++) {
+                r += dir[d][0];
+                c += dir[d][1];
+                res.add(matrix[r][c]);
+            }
+            step[d % 2]--;
+            d = (d + 1) % 4;
+        }
+        return res;
+    }
+}
