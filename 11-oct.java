1eb85e03011d119a198d4c999dd14efe586fe661
Gowri Chandana PB <gowrichandana85@gmail.com>
2025-10-12 00:36:13 +0530

    Set Matrix Zeroes

--- 11-oct.java
+++ 11-oct.java
@@ -42,3 +42,31 @@ class Solution {
         return res;
     }
 }
+
+
+// 3. Set Matrix Zeroes
+
+class Solution {
+    public void setZeroes(int[][] matrix) {
+        int row = matrix.length, col = matrix[0].length;
+        boolean[] rowZero = new boolean[row];
+        boolean[] colZero = new boolean[col];
+
+        for (int r = 0; r < row; r++) {
+            for (int c = 0; c < col; c++) {
+                if (matrix[r][c] == 0) {
+                    rowZero[r] = true;
+                    colZero[c] = true;
+                }
+            }
+        }
+
+        for (int r = 0; r < row; r++) {
+            for (int c = 0; c < col; c++) {
+                if (rowZero[r] || colZero[c]) {
+                    matrix[r][c] = 0;
+                }
+            }
+        }
+    }
+}
