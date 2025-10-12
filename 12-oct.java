da980f1d9b99b7f509bff169f129d992ed5e4539
Gowri Chandana PB <gowrichandana85@gmail.com>
2025-10-12 20:38:21 +0530

    Pow(x, n)

--- 12-oct.java
+++ 12-oct.java
@@ -1,1 +1,13 @@
-// 1. 
+// 1. Pow(x, n)
+
+class Solution {
+    public double myPow(double x, int n) {
+        if(x == 0) return 0;
+        if(n == 0) return 1;
+        double res = 1;
+        for(int i=0; i<Math.abs(n); i++){
+            res *= x;
+        }
+        return n >= 0 ? res : 1 / res;
+    }
+}
