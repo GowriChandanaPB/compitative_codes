04a4cf8bb7f3e01c3af38b1dc26f1e4db91a36d1
Gowri Chandana PB <gowrichandana85@gmail.com>
2025-10-14 22:34:31 +0530

    Reorder List

--- 14-oct.java
+++ 14-oct.java
@@ -16,3 +16,30 @@ class Solution {
 }
 
 
+// 2. Reorder List
+
+class Solution {
+    public void reorderList(ListNode head) {
+        head = reorder(head, head.next);
+    }
+    public ListNode reorder(ListNode root, ListNode cur){
+        if(cur == null){
+            return root;
+        }
+        root = reorder(root, cur.next);
+        if(root == null){
+            return null;
+        }
+        ListNode temp = null;
+        if(root == cur || root.next == cur){
+            cur.next = null;
+        } else {
+            temp = root.next;
+            root.next = cur;
+            cur.next = temp;
+        }
+        return temp;
+    }
+}
+
+
