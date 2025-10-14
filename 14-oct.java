b0bd5753a5e3fd0cbd54605ffa42dc9ccd9e46ae
Gowri Chandana PB <gowrichandana85@gmail.com>
2025-10-14 22:33:52 +0530

    Linked List Cycle

--- 14-oct.java
+++ 14-oct.java
@@ -1,1 +1,18 @@
+// 1. Linked List Cycle   
+
+class Solution {
+    public boolean hasCycle(ListNode head) {
+        HashSet<ListNode> seen = new HashSet<>();
+        ListNode temp = head;
+        while(temp != null){
+            if(seen.contains(temp)){
+                return true;
+            }
+            seen.add(temp);
+            temp = temp.next;
+        }
+        return false;
+    }
+}
+
 
