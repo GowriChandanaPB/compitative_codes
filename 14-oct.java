88604939c5bea1bf4cc093db3b07460f9f712e98
Gowri Chandana PB <gowrichandana85@gmail.com>
2025-10-14 22:37:29 +0530

    LRU Cache

--- 14-oct.java
+++ 14-oct.java
@@ -91,7 +91,7 @@ class Solution {
 }
 
 
-// 8. Merge k Sorted Lists
+// 6. Merge k Sorted Lists
 
 class Solution {
     public ListNode mergeKLists(ListNode[] lists) {
@@ -111,5 +111,30 @@ class Solution {
             temp = temp.next;
         }
         return res.next;
+    }
+}
+
+
+// 7. LRU Cache
+
+class LRUCache {
+    private final Map<Integer, Integer> cache;
+    private final int capacity;
+
+    public LRUCache(int capacity) {
+        this.capacity = capacity;
+        this.cache = new LinkedHashMap<>(capacity, 0.75f, true) {
+            protected boolean removeEldestEntry(Map.Entry<Integer, Integer> eldest) {
+                return size() > LRUCache.this.capacity;
+            }
+        };
+    }
+
+    public int get(int key) {
+        return cache.getOrDefault(key, -1);
+    }
+
+    public void put(int key, int value) {
+        cache.put(key, value);
     }
 }
