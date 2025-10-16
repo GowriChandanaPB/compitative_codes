5f47593662950a3e880b3dfbe9e3d9cb4c2efa9d
Gowri Chandana PB <gowrichandana85@gmail.com>
2025-10-16 23:57:54 +0530

    Maximum Subarray

--- 16-oct.java
+++ 16-oct.java
@@ -1,1 +1,15 @@
-// 1. 
+// 1. Maximum Subarray
+
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int sub = nums[0], sum = 0;
+        for (int i : nums) {
+            if (sum < 0) {
+                sum = 0;
+            }
+            sum += i;
+            sub = Math.max(sub, sum);
+        }
+        return sub;
+    }
+}
