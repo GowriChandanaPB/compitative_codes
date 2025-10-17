8f5e29f0392059d4fb09788132a7c1a2d2ee39bd
Gowri Chandana PB <gowrichandana85@gmail.com>
2025-10-17 18:08:57 +0530

    Target Sum

--- 17-oct.java
+++ 17-oct.java
@@ -10,3 +10,20 @@ class Solution {
         return dp[amount];
     }
 }
+
+
+// 2. Target Sum
+
+class Solution {
+    public int findTargetSumWays(int[] nums, int target) {
+        return backtrack(0, 0, nums, target);
+    }
+
+    private int backtrack(int i, int total, int[] nums, int target) {
+        if (i == nums.length) {
+            return total == target ? 1 : 0;
+        }
+        return backtrack(i + 1, total + nums[i], nums, target) +
+               backtrack(i + 1, total - nums[i], nums, target);
+    }
+}
