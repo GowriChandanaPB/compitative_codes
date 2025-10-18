d9e9534cfe5fc5553034c97df71b92af06b68019
Gowri Chandana PB <gowrichandana85@gmail.com>
2025-10-18 17:48:11 +0530

    Jump Game II

--- 19oct.java
+++ 19oct.java
@@ -123,3 +123,23 @@ class Solution {
         return goal == 0;
     }
 }
+
+
+// 6. Jump Game II
+
+class Solution {
+    public int jump(int[] nums) {
+        int res = 0, l = 0, r = 0;
+
+        while (r < nums.length - 1) {
+            int farthest = 0;
+            for (int i = l; i <= r; i++) {
+                farthest = Math.max(farthest, i + nums[i]);
+            }
+            l = r + 1;
+            r = farthest;
+            res++;
+        }
+        return res;
+    }
+}
