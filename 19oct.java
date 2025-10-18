56645851861795c9319d34389ab59acd89b5debf
Gowri Chandana PB <gowrichandana85@gmail.com>
2025-10-18 23:46:51 +0530

    Partition Labels

--- 19oct.java
+++ 19oct.java
@@ -211,3 +211,28 @@ class Solution {
         return good.size() == 3;
     }
 }
+
+
+// 10. Partition Labels
+
+ class Solution {
+    public List<Integer> partitionLabels(String s) {
+        Map<Character, Integer> last = new HashMap<>();
+        for (int i = 0; i < s.length(); i++) {
+            last.put(s.charAt(i), i);
+        }
+
+        List<Integer> res = new ArrayList<>();
+        int size = 0, end = 0;
+        for (int i = 0; i < s.length(); i++) {
+            size++;
+            end = Math.max(end, last.get(s.charAt(i)));
+
+            if (i == end) {
+                res.add(size);
+                size = 0;
+            }
+        }
+        return res;
+    }
+}
