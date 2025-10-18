672b48340b77935b65303a31f94ea95cec6e9769
Gowri Chandana PB <gowrichandana85@gmail.com>
2025-10-18 17:40:14 +0530

    Burst Balloons

--- 19oct.java
+++ 19oct.java
@@ -53,3 +53,30 @@ class Solution {
         return dp[0];
     }
 }
+
+
+// 3. Burst Balloons
+
+class Solution {
+    public int maxCoins(int[] nums) {
+        int n = nums.length;
+        int[] newNums = new int[n + 2];
+        newNums[0] = newNums[n + 1] = 1;
+        for (int i = 0; i < n; i++) {
+            newNums[i + 1] = nums[i];
+        }
+
+        int[][] dp = new int[n + 2][n + 2];
+        for (int l = n; l >= 1; l--) {
+            for (int r = l; r <= n; r++) {
+                for (int i = l; i <= r; i++) {
+                    int coin = newNums[l - 1] * newNums[i] * newNums[r + 1];
+                    coin += dp[l][i - 1] + dp[i + 1][r];
+                    dp[l][r] = Math.max(dp[l][r], coin);
+                }
+            }
+        }
+
+        return dp[1][n];
+    }
+}
