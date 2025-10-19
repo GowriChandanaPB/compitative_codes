78e5ddea45f120aaa7fdb7659be4d530e7f7f002
Gowri Chandana PB <gowrichandana85@gmail.com>
2025-10-19 18:25:07 +0530

    Valid Anagram

--- 19-oct.java
+++ 19-oct.java
@@ -99,3 +99,17 @@ class Solution {
 
     }
 }
+
+
+// 7. Valid Anagram
+
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length() != t.length()) return false;
+        char[] sarr = s.toCharArray();
+        char[] tarr = t.toCharArray();
+        Arrays.sort(sarr);
+        Arrays.sort(tarr);
+        return Arrays.equals(sarr, tarr);
+    }
+}
