a53b4680386e469f64aea1aea51edc6fe0b6d84c
Gowri Chandana PB <gowrichandana85@gmail.com>
2025-10-19 16:25:25 +0530

    Valid Parentheses

--- 19-oct.java
+++ 19-oct.java
@@ -13,3 +13,22 @@ class Solution {
         return new int[]{};
     }
 }
+
+
+// 2. Valid Parentheses
+
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for(int i=0; i<s.length(); i++){
+            char ch = s.charAt(i);
+            if(ch == '(') stack.push(')');
+            else if(ch == '[') stack.push(']');
+            else if(ch == '{') stack.push('}');
+            else{
+                if(stack.isEmpty() || stack.pop() != ch) return false;
+            }
+        }
+        return stack.isEmpty();
+    }
+}
