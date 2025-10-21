2a1a8854e43b5d0c35fd9f7c41adf23de9f4d4ff
Gowri Chandana PB <gowrichandana85@gmail.com>
2025-10-21 20:15:42 +0530

    Implement Queue using Stacks

--- 21-oct.java
+++ 21-oct.java
@@ -13,3 +13,38 @@ class Solution {
         return false;
     }
 }
+
+
+// 2. Implement Queue using Stacks
+
+class MyQueue {
+    private Stack<Integer> ip;
+    private Stack<Integer> op;
+
+    public MyQueue() {
+        ip = new Stack<>();
+        op = new Stack<>();    
+    }
+    
+    public void push(int x) {
+        ip.push(x);
+    }
+    
+    public int pop() {
+        peek();
+        return op.pop();
+    }
+    
+    public int peek() {
+        if(op.isEmpty()){
+            while(!ip.isEmpty()){
+                op.push(ip.pop());
+            }
+        }
+        return op.peek();
+    }
+    
+    public boolean empty() {
+        return ip.isEmpty() && op.isEmpty();
+    }
+}
