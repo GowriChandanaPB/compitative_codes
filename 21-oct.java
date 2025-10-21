e15cf0f05bdb215a5275750aad98e5b66a407c3d
Gowri Chandana PB <gowrichandana85@gmail.com>
2025-10-21 20:33:24 +0530

    First Bad Version

--- 21-oct.java
+++ 21-oct.java
@@ -48,3 +48,18 @@ class MyQueue {
         return ip.isEmpty() && op.isEmpty();
     }
 }
+
+
+// 3. First Bad Version 
+
+public class Solution extends VersionControl {
+    public int firstBadVersion(int n) {
+        int left = 1, right = n;
+        while(left < right){
+            int mid = left + (right - left) / 2;
+            if(isBadVersion(mid)) right = mid;
+            else left = mid + 1;
+        }
+        return left;
+    }
+}
