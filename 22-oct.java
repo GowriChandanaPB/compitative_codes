aeb18609c2ae02b6eece810530276a8c914bdcd9
Gowri Chandana PB <gowrichandana85@gmail.com>
2025-10-22 22:56:56 +0530

    Maximum Subarray

--- 22-oct.java
+++ 22-oct.java
@@ -54,3 +54,19 @@ class Solution {
         return false; 
     }
 }
+
+
+// 5. Maximum Subarray
+
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int sum = 0;
+        int max_sum = nums[0];
+        for(int i=0; i<nums.length; i++){
+            sum += nums[i];
+            if(sum > max_sum) max_sum = sum;
+            if(sum<0) sum = 0;
+        }
+        return max_sum;
+    }
+}
