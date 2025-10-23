b26612bd406d8d641d781b67a5106a9704295b24
Gowri Chandana PB <gowrichandana85@gmail.com>
2025-10-23 23:37:23 +0530

    Length of Longest Substring Without Repeating Characters

--- 23-oct.java
+++ 23-oct.java
@@ -44,3 +44,24 @@ class Solution {
         return Arrays.copyOfRange(points, 0, k);
     }
 }
+
+
+// 3. Length of Longest Substring Without Repeating Characters 
+
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        int start = 0, end = 0, max_len = 0;
+        List<Character> list = new ArrayList<Character>();
+        while(end < s.length()){
+            if(!list.contains(s.charAt(end))){
+                list.add(s.charAt(end));
+                end++;
+                max_len = Math.max(max_len, list.size());
+            } else {
+                list.remove(Character.valueOf(s.charAt(start)));
+                start++;
+            }
+        }
+        return max_len;
+    }
+}
