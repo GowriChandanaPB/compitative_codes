2a8bc0238f9013ccf7e9191c7e134915623379a2
Gowri Chandana PB <gowrichandana85@gmail.com>
2025-10-23 23:34:28 +0530

    K Closest Points to Origin

--- 23-oct.java
+++ 23-oct.java
@@ -33,3 +33,14 @@ class Solution {
         return mat;
     }
 }
+
+
+// 2. K Closest Points to Origin 
+
+class Solution {
+    public int[][] kClosest(int[][] points, int k) {
+        Arrays.sort(points, (a, b) -> (a[0] * a[0] + a[1] * a[1]) -
+                                      (b[0] * b[0] + b[1] * b[1]));
+        return Arrays.copyOfRange(points, 0, k);
+    }
+}
