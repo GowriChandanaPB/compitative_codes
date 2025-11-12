94253cebda3fbb10419eed1ec9cef520cc309832
Gowri Chandana PB <gowrichandana85@gmail.com>
2025-11-12 21:16:50 +0530

    Validate Binary Search Tree

--- 12-nov.java
+++ 12-nov.java
@@ -219,3 +219,20 @@ class MinStack {
         return st.isEmpty() ? -1 : st.get(st.size() - 1)[1];
     }
 }
+
+
+// 9. Validate Binary Search Tree
+
+class Solution {
+    public boolean isValidBST(TreeNode root) {
+        return valid(root, Long.MIN_VALUE, Long.MAX_VALUE);       
+    }
+
+    private boolean valid(TreeNode node, long min, long max) {
+        if (node == null) return true;
+
+        if (!(node.val > min && node.val < max)) return false;
+
+        return valid(node.left, min, node.val) && valid(node.right, node.val, max);
+    }    
+}
