2e61fc2e0edc6019d2740325263b37c2e8a8e5a2
Gowri Chandana PB <gowrichandana85@gmail.com>
2025-11-12 20:59:40 +0530

    Implement Trie (Prefix Tree)

--- 12-nov.java
+++ 12-nov.java
@@ -97,3 +97,52 @@ class Solution {
         return visited == numCourses;
     }
 }
+
+
+// 5. Implement Trie (Prefix Tree)
+
+class TrieNode {
+    boolean end = false;
+    TrieNode[] childs = new TrieNode[26];
+}
+
+class Trie {
+    private TrieNode root;
+
+    public Trie() {
+        root = new TrieNode();
+    }
+
+    public void insert(String word) {
+        TrieNode node = root;
+        for(char c : word.toCharArray()) {
+            int idx = c - 'a';
+            if(node.childs[idx] == null)
+                node.childs[idx] = new TrieNode();
+            node = node.childs[idx];
+        }
+        node.end = true;
+    }
+
+    public boolean search(String word) {
+        TrieNode node = root;
+        for(char c : word.toCharArray()) {
+            int idx = c - 'a';
+            if(node.childs[idx] == null)
+                return false;
+            node = node.childs[idx];
+        }
+        return node.end;
+    }
+
+    public boolean startsWith(String prefix) {
+        TrieNode node = root;
+        for(char c : prefix.toCharArray()) {
+            int idx = c - 'a';
+            if(node.childs[idx] == null)
+                return false;
+            node = node.childs[idx];
+        }
+        return true;
+    }
+}
