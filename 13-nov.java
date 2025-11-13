f37a8b6bd381ad1a3526ffece9571915cbe9229d
Gowri Chandana PB <gowrichandana85@gmail.com>
2025-11-13 22:52:19 +0530

    Spiral Matrix

--- 13-nov.java
+++ 13-nov.java
@@ -164,3 +164,49 @@ public class Solution {
         return res.toArray(new int[res.size()][]);
     }
 }
+
+
+// 7. Lowest Common Ancestor of a Binary Tree
+
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null || root == p || root == q) return root;
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        if (left != null && right != null) return root;
+        return left != null ? left : right;        
+    }
+}
+
+
+// 8. Spiral Matrix
+
+class Solution {
+    public List<Integer> spiralOrder(int[][] matrix) {
+        int row_start = 0;
+        int row_end = matrix.length-1;
+        int col_start = 0;
+        int col_end = matrix[0].length-1;
+        List<Integer> res = new ArrayList<>();
+
+        if(matrix.length == 0) return res;
+        while(row_start <= row_end && col_start <= col_end){
+            for(int i=col_start; i<= col_end; i++) res.add(matrix[row_start][i]);
+            row_start++;
+
+            for(int i=row_start; i<= row_end; i++) res.add(matrix[i][col_end]);
+            col_end--;
+
+            if(row_start <= row_end){
+                for(int i=col_end; i>=col_start; i--) res.add(matrix[row_end][i]);
+            }
+            row_end--;
+
+            if(col_start <= col_end){
+                for(int i=row_end; i>=row_start; i--) res.add(matrix[i][col_start]);
+            }
+            col_start++;
+        }
+        return res;
+    }
+}
