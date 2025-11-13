3222f1b042e3ae0cf847ad227e9e360a14ccf72c
Gowri Chandana PB <gowrichandana85@gmail.com>
2025-11-13 22:42:10 +0530

    Permutations

--- 13-nov.java
+++ 13-nov.java
@@ -113,3 +113,36 @@ class Solution {
         return answer;
     }
 }
+
+
+// 5. Permutations
+
+
+class Solution {
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (nums.length == 1) {
+            List<Integer> single = new ArrayList<>();
+            single.add(nums[0]);
+            res.add(single);
+            return res;
+        }
+        for (int i = 0; i < nums.length; i++) {
+            int n = nums[i];
+            int[] remainNums = new int[nums.length - 1];
+            int index = 0;
+            for (int j = 0; j < nums.length; j++) {
+                if (j != i) {
+                    remainNums[index] = nums[j];
+                    index++;
+                }
+            }            
+            List<List<Integer>> perms = permute(remainNums);
+            for (List<Integer> p : perms) {
+                p.add(n);
+            }   
+            res.addAll(perms);
+        }
+        return res;        
+    }
+}
