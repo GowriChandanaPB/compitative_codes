4d0b0a7fa04b9fb751c736668ae7b29722497f6e
Gowri Chandana PB <gowrichandana85@gmail.com>
2025-11-14 17:24:19 +0530

    Russian Doll Envelopes

--- 14-nov.java
+++ 14-nov.java
@@ -49,3 +49,22 @@ public List<Integer> largestDivisibleSubset(int[] nums) {
 }
 
 }
+
+
+// 3. Russian Doll Envelopes
+
+class Solution {
+    public int maxEnvelopes(int[][] envelopes) {
+        Arrays.sort(envelopes, (a,b) -> a[0] == b[0] ? b[1] - a[1] : a[0] - b[0]);
+        int[] p = new int[envelopes.length];
+        int ans = 0;
+        for (int[] env : envelopes) {
+            int height = env[1];
+            int left = Arrays.binarySearch(p, 0, ans, height);
+            if (left < 0) left = -left - 1;
+            if (left == ans) ans++;
+            p[left] = height;
+        }
+        return ans;
+    }
+}
