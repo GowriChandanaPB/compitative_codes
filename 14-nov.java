6bd6937643880b91b3a37d907f60f7c7dd7633c5
Gowri Chandana PB <gowrichandana85@gmail.com>
2025-11-14 19:00:31 +0530

    Partition Equal Subset Sum

--- 14-nov.java
+++ 14-nov.java
@@ -193,3 +193,27 @@ class Solution {
         return dp[s.length()];        
     }
 }
+
+
+// 8. Partition Equal Subset Sum
+
+class Solution {
+    public boolean canPartition(int[] nums) {
+        int total = Arrays.stream(nums).sum();
+        if (total % 2 != 0) return false;
+
+        int target = total / 2;
+        boolean[] dp = new boolean[target + 1];
+        dp[0] = true;
+
+        for (int n : nums) {
+            for (int i = target; i >= n; i--) {
+                if (dp[i]) continue;
+                if (dp[i - n]) dp[i] = true;
+                if (dp[target]) return true;
+            }
+        }
+
+        return false;        
+    }
+}
