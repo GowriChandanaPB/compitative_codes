bafa73fa75e586a1b9d256a7d721fac72a7fe670
Gowri Chandana PB <gowrichandana85@gmail.com>
2025-11-15 18:15:27 +0530

    4sum

--- 15-nov.java
+++ 15-nov.java
@@ -37,3 +37,37 @@ class Solution {
         return res;
     }
 }
+
+
+// 3. 4sum
+
+class Solution {
+    public List<List<Integer>> fourSum(int[] arr, int target) {
+        Arrays.sort(arr);
+        List<List<Integer>> temp = new ArrayList<>();
+        int n = arr.length;
+        for (int i = 0; i < n - 3; i++) {
+            if (i > 0 && arr[i] == arr[i - 1]) continue;
+            for (int j = i + 1; j < n - 2; j++) {
+                if (j > i + 1 && arr[j] == arr[j - 1]) continue;
+                int p = j + 1;
+                int q = n - 1;
+                while (p < q) {
+                    long sum = (long) arr[i] + arr[j] + arr[p] + arr[q];
+                    if (sum < target) {
+                        p++;
+                    } else if (sum > target) {
+                        q--;
+                    } else {
+                        temp.add(Arrays.asList(arr[i], arr[j], arr[p], arr[q]));
+                        while (p < q && arr[p] == arr[p + 1]) p++;
+                        while (p < q && arr[q] == arr[q - 1]) q--;
+                        p++;
+                        q--;
+                    }
+                }
+            }
+        }
+        return temp;
+    }
+}
