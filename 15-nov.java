a5a8c5081216d9c460bf6fdcabc883c5a59b0558
Gowri Chandana PB <gowrichandana85@gmail.com>
2025-11-15 14:13:44 +0530

    Two Sum II - Input Array Is Sorted

--- 15-nov.java
+++ 15-nov.java
@@ -1,3 +1,12 @@
-public class 15-nov {
-    
+// 1. Two Sum II - Input Array Is Sorted
+
+class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+        int left = 0, right = numbers.length-1;
+        while((numbers[left] + numbers[right]) != target){
+            if((numbers[left] + numbers[right]) < target) ++left;
+            else --right;
+        }
+        return new int[] {left + 1, right + 1};
+    }
 }
