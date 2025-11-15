0f588671d3a305f6d85ddc606f913f43f2834705
Gowri Chandana PB <gowrichandana85@gmail.com>
2025-11-15 20:25:30 +0530

    Boats to Save People

--- 15-nov.java
+++ 15-nov.java
@@ -136,3 +136,30 @@ class Solution {
         return c % 4 != 3;
     }
 }
+
+
+// 7. Boats to Save People
+
+class Solution {
+    public int numRescueBoats(int[] people, int limit) {
+        int boatCount = 0;
+        Arrays.sort(people);
+        
+        int left = 0;
+        int right = people.length - 1;
+        
+        while(left <= right){
+            int sum = people[left] + people[right];
+            if(sum <= limit){
+                boatCount++;
+                left++;
+                right--;
+            }
+            else{
+                boatCount++;
+                right--;
+            }
+        }
+        return boatCount;
+    }
+}
