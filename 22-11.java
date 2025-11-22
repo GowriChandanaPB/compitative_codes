f99550a3a41d332e86fa135cb574a5662fee8440
Gowri Chandana PB <gowrichandana85@gmail.com>
2025-11-22 21:49:34 +0530

    Spiral Matrix

--- 22-11.java
+++ 22-11.java
@@ -20,3 +20,36 @@ class Solution {
         return (int) (sign * res);
     }
 }
+
+
+// 2. Spiral Matrix
+
+class Solution {
+    public List<Integer> spiralOrder(int[][] matrix) {
+        int row_start = 0;
+        int row_end = matrix.length-1;
+        int col_start = 0;
+        int col_end = matrix[0].length-1;
+        List<Integer> res = new ArrayList<>();
+
+        if(matrix.length == 0) return res;
+        while(row_start <= row_end && col_start <= col_end){
+            for(int i=col_start; i<= col_end; i++) res.add(matrix[row_start][i]);
+            row_start++;
+
+            for(int i=row_start; i<= row_end; i++) res.add(matrix[i][col_end]);
+            col_end--;
+
+            if(row_start <= row_end){
+                for(int i=col_end; i>=col_start; i--) res.add(matrix[row_end][i]);
+            }
+            row_end--;
+
+            if(col_start <= col_end){
+                for(int i=row_end; i>=row_start; i--) res.add(matrix[i][col_start]);
+            }
+            col_start++;
+        }
+        return res;
+    }
+}
