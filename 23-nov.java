d396981feba2ca5ad5552225df8dfc3caa75e242
Gowri Chandana PB <gowrichandana85@gmail.com>
2025-11-23 22:49:57 +0530

    Task Scheduler

--- 23-nov.java
+++ 23-nov.java
@@ -46,3 +46,23 @@ class Solution {
     return leaves;
     }
 }
+
+
+// 3. Task Scheduler
+
+class Solution {
+    public int leastInterval(char[] tasks, int n) {
+        int [] count = new int[26];
+        for(char i:tasks){
+            count[i-'A']++;
+        }
+        Arrays.sort(count);
+        int max = count[25];
+        int idle = (max - 1) * n;
+
+        for(int i=24; i>=0; i--){
+            idle -= Math.min(max - 1, count[i]);
+        }
+        return Math.max(0, idle) + tasks.length;
+    }
+}
