09e78ec3585e728782c6a7da149b6e825e4cc476
Gowri Chandana PB <gowrichandana85@gmail.com>
2025-11-23 22:43:08 +0530

    1

--- 23-nov.java
+++ 23-nov.java
@@ -5,21 +5,11 @@ class Solution {
         int[] pcount = new int[26];
         int[] scount = new int[26];
         List<Integer> result = new ArrayList<>();
-
-        for (char c : p.toCharArray()) {
-            pcount[c - 'a']++;
-        }
-
+        for (char c : p.toCharArray()) pcount[c - 'a']++;
         for (int i = 0; i < s.length(); i++) {
             scount[s.charAt(i) - 'a']++;
-
-            if (i >= p.length()) {
-                scount[s.charAt(i - p.length()) - 'a']--;
-            }
-
-            if (Arrays.equals(pcount, scount)) {
-                result.add(i - p.length() + 1);
-            }
+            if (i >= p.length()) scount[s.charAt(i - p.length()) - 'a']--;
+            if (Arrays.equals(pcount, scount)) result.add(i - p.length() + 1);
         }
         return result;
     }
