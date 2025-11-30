b2e6efc8b61ba3d69dc8afed890a7d915e3e5ac6
Gowri Chandana PB <gowrichandana85@gmail.com>
2025-11-30 21:06:26 +0530

    Largest Rectangle in Histogram

--- 30-nov.java
+++ 30-nov.java
@@ -42,3 +42,27 @@ class Solution {
         return res.next;
     }
 }
+
+
+// 3. Largest Rectangle in Histogram
+
+class Solution {
+    public int largestRectangleArea(int[] heights) {
+        Deque<Integer> d = new ArrayDeque<>();
+        int max_area = 0;
+        int n = heights.length;
+
+        for (int i = 0; i <= n; i++) {
+            int curr_len = (i == n) ? 0 : heights[i];
+
+            while (!d.isEmpty() && curr_len < heights[d.peek()]) {
+                int height = heights[d.pop()];
+                int width = d.isEmpty() ? i : i - d.peek() - 1;
+                max_area = Math.max(max_area, height * width);
+            }
+            d.push(i);
+        }
+
+        return max_area;    
+    }
+}
