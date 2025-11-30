e66cf4f6bc53c5035f10471182dd9549531e35c0
Gowri Chandana PB <gowrichandana85@gmail.com>
2025-11-30 21:04:30 +0530

    Merge k Sorted Lists

--- 30-nov.java
+++ 30-nov.java
@@ -18,3 +18,27 @@ class Solution {
         return dp.lastEntry().getValue();
     }
 }
+
+
+// 2. Merge k Sorted Lists
+
+class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+        List<Integer> node = new ArrayList<>();
+        for (ListNode lst : lists) {
+            while (lst != null) {
+                node.add(lst.val);
+                lst = lst.next;
+            }
+        }
+        Collections.sort(node);
+
+        ListNode res = new ListNode(0);
+        ListNode temp = res;
+        for (int i : node) {
+            temp.next = new ListNode(i);
+            temp = temp.next;
+        }
+        return res.next;
+    }
+}
