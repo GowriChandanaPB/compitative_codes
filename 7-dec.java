cb53c8c1927864a22b17f1e750fb6ec0ce9861c4
Gowri Chandana PB <gowrichandana85@gmail.com>
2025-12-07 19:49:41 +0530

    two sum

--- 7-dec.java
+++ 7-dec.java
@@ -1,1 +1,14 @@
-// 1.
+// 1. Two sum
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int left = 0, right = nums.length - 1; 
+        while (left < right) {
+            int sum = nums[left] + nums[right];
+            if (sum == target) return new int[]{left+1, right+1};
+            else if (sum < target) left++;
+            else right--;
+        }
+        return new int[]{0, 0};
+    }
+}
