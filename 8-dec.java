b7c30ac983dbaa3c23d4a7bca4478d3f17d590ce
Gowri Chandana PB <gowrichandana85@gmail.com>
2025-12-08 08:27:27 +0530

    solution

--- 8-dec.java
+++ 8-dec.java
@@ -1,1 +1,14 @@
 // 1. remove duplicate from sorted array
+
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int j = 1;
+        for(int i=0; i<nums.length; i++){
+            if(nums[i] != nums[j-1]){
+                nums[j] = nums[i];
+                j++;
+            }
+        }
+        return j;
+    }
+}
