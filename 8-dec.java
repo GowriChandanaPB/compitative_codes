565b4773e02f1551cf7bd79e86a2f1573aed152d
Gowri Chandana PB <gowrichandana85@gmail.com>
2025-12-08 08:42:20 +0530

    solution

--- 8-dec.java
+++ 8-dec.java
@@ -16,3 +16,17 @@ class Solution {
 
 
 // 2. valid palindrome
+
+class Solution {
+    public boolean isPalindrome(String s) {
+        s = s.toLowerCase();
+        s = s.replaceAll("[^a-z0-9]","");
+        int left = 0, right = s.length()-1;
+        while(left < right){
+            if(s.charAt(left) != s.charAt(right)) return false;
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
