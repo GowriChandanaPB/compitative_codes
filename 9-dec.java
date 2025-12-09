b25e4c1f8a83420fb51ba95f28833bffa8e0886a
Gowri Chandana PB <gowrichandana85@gmail.com>
2025-12-09 07:36:59 +0530

    happy number

--- 9-dec.java
+++ 9-dec.java
@@ -49,3 +49,28 @@ class Solution {
         return slow;
     }
 }
+
+
+// 4. Happy Number
+
+class Solution {
+    public boolean isHappy(int n) {
+        int slow = square(n);
+        int fast = square(square(n));
+        while(slow != fast){
+            slow = square(slow);
+            fast = square(square(fast));
+            if(slow == 1 || fast == 1) return true;
+        }
+        return slow == 1;
+    }
+    public int square(int n){
+        int res = 0;
+        while(n > 0){
+            int x = n%10;
+            res += x*x;
+            n = n/10;
+        }
+        return res;
+    }
+}
