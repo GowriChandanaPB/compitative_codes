59ae7daa042f8efbc005e502843aee2b11623938
Gowri Chandana PB <gowrichandana85@gmail.com>
2025-12-11 00:03:20 +0530

    linked list cycle

--- 10-dec.java
+++ 10-dec.java
@@ -55,3 +55,18 @@ class Solution {
         }        
     }
 }
+
+
+// 3. Linked List Cycle
+
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        ListNode slow = head, fast = head;
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+            if(slow == fast) return true;
+        }
+        return false;
+    }
+}
