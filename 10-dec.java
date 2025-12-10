2d4a1f9b9d1def7e7566f3d4386b9262a02c09b7
Gowri Chandana PB <gowrichandana85@gmail.com>
2025-12-10 23:14:47 +0530

    remove Nth Node From End of List

--- 10-dec.java
+++ 10-dec.java
@@ -1,2 +1,17 @@
-// 1. 
+// 1. remove Nth Node From End of List
+
+class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode temp = new ListNode(-1);
+        temp.next = head;
+        ListNode f = temp, s = temp; 
+        for(int i=0; i<n; i++) s = s.next;
+        while(s.next != null){
+            f = f.next;
+            s = s.next;
+        }
+        f.next = f.next.next;
+        return temp.next;
+    }
+}
 
