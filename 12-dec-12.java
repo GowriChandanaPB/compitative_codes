a15b22486e45f1a1eaeb013be673a2ad95b1d4a7
Gowri Chandana PB <gowrichandana85@gmail.com>
2025-12-13 00:22:47 +0530

    Container With Most Water

--- 12-dec-12.java
+++ 12-dec-12.java
@@ -75,3 +75,23 @@ class Solution {
                 new String(chS, stridx, minLen);
     }
 }
+
+
+
+// 4. Container With Most Water
+
+class Solution {
+    public int maxArea(int[] heights) {
+        int l = 0;
+        int r = heights.length - 1;
+        int res = 0;
+
+        while (l < r) {
+            int area = Math.min(heights[l], heights[r]) * (r - l);
+            res = Math.max(res, area);
+            if (heights[l] <= heights[r]) l++;
+            else r--;
+        }
+        return res;
+    }
+}
