d590c4efc0f29ccc82304a4e5583369fd91bc55a
Gowri Chandana PB <gowrichandana85@gmail.com>
2025-12-14 23:52:59 +0530

    Fraction Addition and Subtraction

--- 14-dec.java
+++ 14-dec.java
@@ -153,3 +153,24 @@ class Solution {
                 new String(chS, stridx, minLen);
     }
 }
+
+
+// 8. Fraction Addition and Subtraction
+
+public String fractionAddition(String expression) {
+    Scanner sc = new Scanner(expression).useDelimiter("/|(?=[-+])");
+    int A = 0, B = 1;
+    while (sc.hasNext()) {
+        int a = sc.nextInt(), b = sc.nextInt();
+        A = A * b + a * B;
+        B *= b;
+        int g = gcd(A, B);
+        A /= g;
+        B /= g;
+    }
+    return A + "/" + B;
+}
+
+private int gcd(int a, int b) {
+    return a != 0 ? gcd(b % a, a) : Math.abs(b);
+}
