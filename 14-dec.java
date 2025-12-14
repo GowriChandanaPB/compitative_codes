7f1cf2798a0c35acc9e5f4ff7499fa16572abe3b
Gowri Chandana PB <gowrichandana85@gmail.com>
2025-12-14 23:44:37 +0530

    group anagrams

--- 14-dec.java
+++ 14-dec.java
@@ -108,4 +108,18 @@ class Solution {
 
 
 
-// 6.
+// 6. group anagrams 
+
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+        for(String i : strs){
+        char[] c = i.toCharArray();
+        Arrays.sort(c);
+        String s = new String(c);
+        if(!map.containsKey(s)) map.put(s, new ArrayList<>());
+        map.get(s).add(i);
+        }
+        return new ArrayList<>(map.values());
+        }
+}
