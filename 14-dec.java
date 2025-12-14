e94725b04d421c626c3245ded1a5fcdc8269aedf
Gowri Chandana PB <gowrichandana85@gmail.com>
2025-12-14 23:49:12 +0530

    Minimum Window Substring

--- 14-dec.java
+++ 14-dec.java
@@ -123,3 +123,33 @@ class Solution {
         return new ArrayList<>(map.values());
         }
 }
+
+
+
+// 7. Minimum Window Substring
+
+
+class Solution {
+    public String minWindow(String s, String t) {
+        if (s == null || t == null || s.length() == 0 || t.length() == 0 || s.length() < t.length()) return new String();
+        int[] map = new int[128];
+        int count = t.length();
+        int start = 0, end = 0, minLen = Integer.MAX_VALUE, stridx = 0;
+        for (char c : t.toCharArray()) map[c]++;
+
+        char[] chS = s.toCharArray();
+
+        while (end < chS.length) {
+            if (map[chS[end++]]-- > 0) count--;
+            while (count == 0) {
+                if (end - start < minLen) {
+                    stridx = start;
+                    minLen = end - start;
+                }
+                if (map[chS[start++]]++ == 0) count++;
+            }
+        }
+        return minLen == Integer.MAX_VALUE ? new String() :
+                new String(chS, stridx, minLen);
+    }
+}
