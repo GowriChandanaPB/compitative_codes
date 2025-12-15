571318ee3d5979c2d72fe0841cbb946682edc1eb
Gowri Chandana PB <gowrichandana85@gmail.com>
2025-12-15 08:15:45 +0530

    Remove K Digits

--- 15-dec.java
+++ 15-dec.java
@@ -44,5 +44,33 @@ class Solution {
 }
 
 
-// 3. 
+
+// 3. Remove K Digits 
+
+
+class Solution {
+    public String removeKdigits(String num, int k) {
+        if(k == num.length()) return "0";
+        Stack<Character> stk = new Stack<>();
+        int i = 0;
+        while(i < num.length()){
+            while(k > 0 && !stk.isEmpty() && stk.peek() > num.charAt(i)){
+                stk.pop();
+                k--;
+            }
+            stk.push(num.charAt(i));
+            i++;
+        } 
+        while(k > 0){
+            stk.pop();
+            k--;
+        }
+        StringBuilder sb = new StringBuilder();
+        while(!stk.isEmpty()) sb.append(stk.pop());
+        sb.reverse();
+        while(sb.length() > 1 && sb.charAt(0)=='0') sb.deleteCharAt(0);
+        return sb.toString();
+
+    }
+}
 
