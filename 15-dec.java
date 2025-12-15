49f612ce7dae9b886434dee598c980e89a116c99
Gowri Chandana PB <gowrichandana85@gmail.com>
2025-12-15 08:34:01 +0530

    Bulls and Cows

--- 15-dec.java
+++ 15-dec.java
@@ -76,5 +76,25 @@ class Solution {
 
 
 
-// 4.
+// 4. Bulls and Cows
 
+
+class Solution {
+    public String getHint(String secret, String guess) {
+    int bull = 0;
+    int cow = 0;
+    int[] num = new int[10];
+    for (int i = 0; i<secret.length(); i++) {
+        int s = Character.getNumericValue(secret.charAt(i));
+        int g = Character.getNumericValue(guess.charAt(i));
+        if (s == g) bull++;
+        else {
+            if (num[s] < 0) cow++;
+            if (num[g] > 0) cow++;
+            num[s] ++;
+            num[g] --;
+        }
+    }
+    return bull + "A" + cow + "B";
+}
+}
