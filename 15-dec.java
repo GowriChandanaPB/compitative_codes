dff71a4887f16bd933930ef8a63756c50bcac7d4
Gowri Chandana PB <gowrichandana85@gmail.com>
2025-12-15 16:57:02 +0530

    Reverse Nodes in k-Group

--- 15-dec.java
+++ 15-dec.java
@@ -200,3 +200,28 @@ class Solution {
         return node;        
     }
 }
+
+
+// 9. Reverse Nodes in k-Group
+
+class Solution {
+    public ListNode reverseKGroup(ListNode head, int k) {
+        ListNode temp = head;
+        int grp = 0;
+        while(temp != null && grp < k){
+            temp = temp.next;
+            grp++;
+        }
+        if(grp == k){
+            temp = reverseKGroup(temp, k);
+            while(grp -->0){
+                ListNode cur = head.next;
+                head.next = temp;
+                temp = head;
+                head = cur;
+            }
+            head = temp;
+        }
+        return head;
+    }
+}
