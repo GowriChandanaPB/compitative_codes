75a53e07aab47e12352c29380fdee5e4935990b6
Gowri Chandana PB <gowrichandana85@gmail.com>
2025-12-15 11:13:33 +0530

    Reverse Words in a String

--- 15-dec.java
+++ 15-dec.java
@@ -163,3 +163,21 @@ class Solution {
         }
     }    
 }
+
+
+
+// 7. Reverse Words in a String
+
+class Solution {
+    public String reverseWords(String s) {
+        String[] words = s.split("\\s+");
+        StringBuilder res = new StringBuilder();
+        for(int i=words.lenght-1; i>=0; i--){
+            res.append(words[i]);
+            if(i != 0){
+                res.append(" ");
+            }
+        }
+        return res.toString().trim();
+    }
+}
