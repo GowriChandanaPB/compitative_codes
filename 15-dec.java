72b933813c21ad3143ca410e7c328f9c9dbda950
Gowri Chandana PB <gowrichandana85@gmail.com>
2025-12-15 17:13:29 +0530

    Longest Palindromic Substring

--- 15-dec.java
+++ 15-dec.java
@@ -225,3 +225,30 @@ class Solution {
         return head;
     }
 }
+
+
+
+// 10. Longest Palindromic Substring
+
+
+class Solution {
+    int lo, maxlen;
+    public String longestPalindrome(String s) {
+        if(s.length() < 2) return s;
+        for(int i=0; i<s.length()-1; i++){
+            extend_p(s, i, i);
+            extend_p(s, i, i+1);
+        }
+        return s.substring(lo, lo + maxlen);
+    }
+    public void extend_p(String s, int j, int k){
+        while(j >= 0 && k<s.length() && s.charAt(j) == s.charAt(k)){
+        j--;
+        k++;
+    }
+    if(maxlen < k - j - 1){
+        lo = j + 1;
+        maxlen = k - j - 1;
+    }
+    }
+}
