c634e92c72d69775993bbb87eb52cef237efa615
Gowri Chandana PB <gowrichandana85@gmail.com>
2025-12-18 22:33:58 +0530

    2 sum

--- 18-dec.java
+++ 18-dec.java
@@ -24,3 +24,21 @@ class Solution {
         return Arrays.equals(s_new, t_new);
     }
 }
+
+
+
+// 3. 2 sum
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int i=0; i<nums.length; i++){
+            int temp = target - nums[i];
+            if(map.containsKey(temp)){
+                return new int[] {map.get(temp), i};
+            }
+            map.put(nums[i], i);
+        }
+        return new int[]{};
+    }
+}
