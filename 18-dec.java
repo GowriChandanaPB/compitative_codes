edaabb8401bf4f1ca012a355ebc64b1d85c56a92
Gowri Chandana PB <gowrichandana85@gmail.com>
2025-12-18 22:38:35 +0530

    Group Anagrams

--- 18-dec.java
+++ 18-dec.java
@@ -42,3 +42,21 @@ class Solution {
         return new int[]{};
     }
 }
+
+
+// 4. Group Anagrams
+
+
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+        for(String i : strs){
+        char[] c = i.toCharArray();
+        Arrays.sort(c);
+        String s = new String(c);
+        if(!map.containsKey(s)) map.put(s, new ArrayList<>());
+        map.get(s).add(i);
+        }
+        return new ArrayList<>(map.values());
+        }
+}
