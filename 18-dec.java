1687fde5644d2c83313374402f784bde2247f09d
Gowri Chandana PB <gowrichandana85@gmail.com>
2025-12-18 22:24:22 +0530

    valid Anagram

--- 18-dec.java
+++ 18-dec.java
@@ -10,3 +10,17 @@ class Solution {
         return false;
     }
 }
+
+
+// 2. valid Anagram
+
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length() != t.length()) return false;
+        char[] s_new = s.toCharArray();
+        char[] t_new = t.toCharArray();
+        Arrays.sort(s_new);
+        Arrays.sort(t_new);
+        return Arrays.equals(s_new, t_new);
+    }
+}
