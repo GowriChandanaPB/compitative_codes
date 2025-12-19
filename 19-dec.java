4ab4e527235e89529e29a516bbf4833fd807e85a
Gowri Chandana PB <gowrichandana85@gmail.com>
2025-12-19 14:40:44 +0530

    Palindrome Number

---
+++ 19-dec.java
@@ -0,0 +1,15 @@
+// 1. Palindrome Number
+
+class Solution {
+    public boolean isPalindrome(int x) {
+        if (x < 0) return false;
+        int og = x;
+        int rev = 0;
+        while (x > 0) {
+            int digit = x % 10;
+            rev = rev * 10 + digit;
+            x /= 10;
+        }
+        return og == rev;
+    }
+}
