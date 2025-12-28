361e0a1b36c98a0b1c861cb8f3766414b8c46f6e
Gowri Chandana PB <gowrichandana85@gmail.com>
2025-12-28 12:17:48 +0530

    1. Exclusive Time of Functions

--- 28-dec.java
+++ 28-dec.java
@@ -1,1 +1,24 @@
+// 1. Exclusive Time of Functions
 
+class Solution {
+    public int[] exclusiveTime(int n, List<String> logs) {
+        int[] result = new int[n];
+        Stack<Integer> stk = new Stack<>();
+        int prev = 0;
+        for(String log : logs){
+            String[] parts = log.split(":");
+            int id = Integer.parseInt(parts[0]);
+            String type = parts[1];
+            int time = Integer.parseInt(parts[2]);
+            if(type.equals("start")){
+                if(!stk.isEmpty()) result[stk.peek()] += time - prev;
+                stk.push(id);
+                prev = time;
+            } else {
+                result[stk.pop()] += time - prev + 1;
+                prev = time + 1;
+            }
+        }
+        return result;
+    }
+}
